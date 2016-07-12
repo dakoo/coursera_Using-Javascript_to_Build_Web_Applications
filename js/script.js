9f790f871b1f453fb8fb70c80eb29d254adfb16b
dakoo <hochul.shin0@gmail.com>
2016-07-12 14:35:57 +0900

    [Text Input handler] If "student" text receives, change the title of the page

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 function sayHello () {
   //Get value of the text input box from #name
   var name = document.getElementById("name").value;
-  
+
   //Make a output message in html
   var message = "<h2>Hello " + name + "!</h2>";
 
   //Put the message at #content
   document.getElementById("content").innerHTML = message;
+
+    //In case of "student" text input, change the value of #title
+  if (name === "student") {
+    //Get the current text of #title
+    var title = document.querySelector("#title").textContent;
+    //Make a output message in html
+    title += " & Lovin' it!";
+    //Put the message at h1 
+    document.querySelector("h1").textContent = title;
+  }
 }
