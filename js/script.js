b57dbcd1f23817e92154d0278a549d1341e733af
dakoo <hochul.shin0@gmail.com>
2016-07-12 14:35:04 +0900

    [Text Input handler] Print a message when a text message received

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@
 function sayHello () {
   //Get value of the text input box from #name
   var name = document.getElementById("name").value;
+  
+  //Make a output message in html
+  var message = "<h2>Hello " + name + "!</h2>";
+
+  //Put the message at #content
+  document.getElementById("content").innerHTML = message;
 }
